d2b8c3a3d866516a676da068b6cae8cbd06172e6
tsp-Ta1wan <louay.belkhamsa@telecom-sudparis.eu>
2024-09-26 23:49:46 +0200

    added fix for Arsenal & Piece

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,80 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Piece;
+use App\Entity\Arsenal;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
-    public function load(ObjectManager $manager): void
+    public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        // Load for Arsenal
+        $this->loadArsenals($manager);
 
+        // Load for Pieces (of war mind you)
+        $this->loadPieces($manager);
+    }
+
+    private function loadArsenals(ObjectManager $manager)
+    {
+        foreach ($this->getArsenalsData() as [$description]) {
+            $arsenal = new Arsenal();
+            // if description is null (which will be soon hehe) i don't know what happens for setDescription. 
+            // Description isn't nullable now we'll comment this for now
+            // Also better have it null in database and displayed differently so it wouldn't take up space
+            /*
+            if (!$description){
+                $arsenal->setDescription('Thou hast no description as yet!');
+            }
+            else {**/
+            $arsenal->setDescription($description);
+            //}
+            $manager->persist($arsenal);
+        }
+    
+        // Flush
         $manager->flush();
+    }
+
+    private function loadPieces(ObjectManager $manager)
+    {
+        // Fetching Arsenals
+        $arsenals = $manager->getRepository(Arsenal::class)->findAll();
+
+        foreach ($this->getPiecesData() as [$name, $description, $type, $acquired, $era, $arsenalIndex]) {
+            $piece = new Piece();
+            $piece->setName($name)
+                ->setDescription($description)
+                ->setType($type)
+                ->setAcquired(new \DateTime($acquired))
+                ->setEra($era)
+                ->setArsenal($arsenals[$arsenalIndex]);  // The OneToMany relation is at play here
+
+            $manager->persist($piece);
+        }
+        
+        $manager->flush();
+    }
+
+    private function getArsenalsData()
+    {
+        // Arsenal data (only description for now)
+        yield ['Medieval Armory'];
+        yield ['No description']; // The null value didn't work, description doesn't appear to be nullable thus far
+        yield ['Viking Weapons'];
+        yield ['Mythical Shields'];
+        yield ['Renaissance Blades'];
+    }
+
+    private function getPiecesData()
+    {
+        // Piece data = [name, description, type, acquired date, era, arsenal index];
+        yield ['Ancient Sword', 'A sword from the medieval era', 'Sword', '2020-05-15', 'Medieval', 0];
+        yield ['Elven Bow', 'A handcrafted bow by elves', 'Bow', '2018-08-12', 'Fantasy', 1];
+        yield ['Steel Dagger', 'A sharp steel dagger', 'Dagger', '2022-03-10', 'Renaissance', 4];
+        yield ['Dragon Shield', 'A shield with a dragon emblem', 'Shield', '2021-07-19', 'Mythical', 3];
+        yield ['Battle Axe', 'A heavy battle axe used in war', 'Axe', '2019-11-23', 'Viking', 2];
     }
 }
